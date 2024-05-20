ace48376f8fcfa992e864f448243741138258f06
Wallace Camargogit config --global user.name Wallace <wallacecpdg@gmail.com>
2024-05-20 09:51:56 +0100

    adjust query sqlserver

--- query/sqlserver.sql
+++ query/sqlserver.sql
@@ -1,5 +1,5 @@
 -- Create the customer registration table on SQL Server
-CREATE TABLE CustomerRegistration (
+CREATE TABLE youtube.dbo.CustomerRegistration (
     CustomerID INT PRIMARY KEY,
     Name VARCHAR(100),
     Email VARCHAR(100),
@@ -7,7 +7,7 @@ CREATE TABLE CustomerRegistration (
 );
 
 -- Insert data into the customer registration table
-INSERT INTO CustomerRegistration (CustomerID, Name, Email, RegistrationDate) VALUES
+INSERT INTO  youtube.dbo.CustomerRegistration (CustomerID, Name, Email, RegistrationDate) VALUES
 (1, 'John Smith', 'john.smith@example.com', '2023-01-15'),
 (2, 'Maria Johnson', 'maria.johnson@example.com', '2023-02-10'),
 (3, 'Carlos Brown', 'carlos.brown@example.com', '2023-03-05');
